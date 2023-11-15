e6dea6d164756b5b2e94661c8269dd14f0a024c0
Johannes Bester <bester.jpb@gmail.com>
2023-11-15 14:53:30 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+	kotlin("jvm") version "1.9.20"
+	application
+	`maven-publish`
+}
+
+group = "za.co.zone"
+version = "1.0-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+group = "za.co.zone"
+version = "1.0"
+
+dependencies {
+
+	implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
+
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
+
+kotlin {
+	jvmToolchain(jdkVersion = 17)
+}
+
+application {
+	mainClass.set("MainKt")
+}
+
+publishing {
+	publications {
+		create<MavenPublication>(name = "release") {
+			groupId = "com.github.Morons"
+			artifactId = "kotlin-library"
+			version = "1.1"
+
+			from(components["java"])
+		}
+	}
+}
---
+++ src/main/kotlin/KotlinDateUtils.kt
@@ -0,0 +1,44 @@
+import kotlinx.datetime.*
+import kotlinx.datetime.Clock.System.now
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
+
+private const val MILLIS_IN_A_SECOND: Int = 1000
+
+private val currentMoment: Instant = now()  // Current Clock time of Device
+private val currentMomentUtc: Instant = now()
+
+//private val currentMomentUtc: Instant = currentMoment.toLocalDateTime(TimeZone.UTC).toInstant(TimeZone.UTC)
+private val datetimeInUtc: LocalDateTime = currentMoment.toLocalDateTime(TimeZone.UTC)
+private val dateInUtc: LocalDate = datetimeInUtc.date
+private val datetimeInSystemZone: LocalDateTime = currentMoment.toLocalDateTime(TimeZone.currentSystemDefault())
+private val monthNumber: Int = dateInUtc.monthNumber
+private val dayOfMonthNumber: Int = dateInUtc.dayOfMonth
+private val yearNumber: Int = dateInUtc.year
+private val time: LocalTime = datetimeInUtc.time
+private val firstDayUtc: Instant = Instant.parse(
+	isoString = "${yearNumber.toString().padStart(length = 4, padChar = '0')}-${
+		monthNumber.toString().padStart(length = 2, padChar = '0')
+	}-01T00:00:00Z"
+)
+private val nextMonth: Instant = firstDayUtc.plus(value = 1, unit = DateTimeUnit.MONTH, timeZone = TimeZone.UTC)
+private val lastDay: Instant = nextMonth.minus(value = 1, unit = DateTimeUnit.DAY, timeZone = TimeZone.UTC)
+private val durationToNextMonth: Duration = nextMonth - currentMoment
+fun Instant.dateTimePeriod(): DateTimePeriod = this.periodUntil(now(), TimeZone.UTC)
+fun nowToLongSystemDefault(): Long =
+	datetimeInSystemZone.toInstant(TimeZone.currentSystemDefault()).toEpochMilliseconds()
+
+fun currentMoment() = currentMoment
+fun getCurrentTimeInSeconds(): Int = currentMomentUtc().epochSeconds.toInt()
+fun currentDayFormatted() =
+	with(currentMoment().toLocalDateTime(TimeZone.currentSystemDefault())) { "$year $month $dayOfMonth" }
+
+fun currentMomentUtc() = currentMomentUtc
+fun nowToLongUtc(): Long = datetimeInUtc.toInstant(TimeZone.UTC).toEpochMilliseconds()
+fun nextMonthUtc() = currentMoment + durationToNextMonth
+fun firstDayUtc() = firstDayUtc
+fun lastDayUtc() = lastDay
+fun dateInUtc() = dateInUtc
+fun in30DaysUtc() = datetimeInUtc.toInstant(TimeZone.UTC) + 30.days
+fun past15DaysUtc() = (datetimeInUtc.toInstant(TimeZone.UTC) - 15.days)
+fun past60DaysUtc() = (datetimeInUtc.toInstant(TimeZone.UTC) - 60.days)
