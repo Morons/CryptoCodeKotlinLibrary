978d1d48fd7ae104d8243f8fe769240707941390
Johannes Bester <bester.jpb@gmail.com>
2023-11-21 14:39:43 +0200

    Fixed getResourceProperty() test

--- src/test/kotlin/ResourcePropertiesUtilsTest.kt
+++ src/test/kotlin/ResourcePropertiesUtilsTest.kt
@@ -1,12 +1,22 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class ResourcePropertiesUtilsTest {
 
 	@Test
 	fun getResourceProperty() {
-	}
-
-	@Test
-	fun setResourceProperty() {
+		setResourceProperty(
+			key = "greeting",
+			value = "Hello World",
+			filePath = "src/main/resources/",
+			fileName = "test.properties"
+		)
+		val expected = "Hello World"
+		val property = getResourceProperty(
+			key = "greeting",
+			filePath = "src/main/resources/",
+			fileName = "test.properties"
+		) ?: ""
+		assertEquals(expected = expected, property)
 	}
 }
